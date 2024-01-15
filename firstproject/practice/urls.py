3f0ac4673f0ebdf8b9176d962cc85a1089105e91
YapJack7890 <108270479+YapJack7890@users.noreply.github.com>
2024-01-15 23:18:25 +0800

    Update urls.py

--- firstproject/practice/urls.py
+++ firstproject/practice/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('register', views.registerPage, name='register'),
     path('home', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('', views.loginPage, name='login'),
+    path('student', views.studentPage, name='student'),
+    path('request', views.RequestPage, name='request'),
+    path('fooditem', views.FoodItemPage, name='fooditem'),
+    path('menu', views.menu, name='menu'),
 ]

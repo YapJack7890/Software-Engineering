b6dc95c1efa04c9b105edb0b94bb2296cbee78b0
YapJack7890 <108270479+YapJack7890@users.noreply.github.com>
2024-01-21 13:17:41 +0800

    Update urls.py

--- djangoenv/firstproject/practice/urls.py
+++ djangoenv/firstproject/practice/urls.py
@@ -3,12 +3,19 @@ from . import views
 
 urlpatterns = [
     path('register', views.registerPage, name='register'),
+    path('logout', views.logoutUser, name='logout'),
     path('home', views.home, name='home'),
     path('', views.loginPage, name='login'),
     path('student', views.studentPage, name='student'),
     path('request', views.RequestPage, name='request'),
-    path('fooditem/<str:pk>/', views.FoodItemPage, name='fooditem'),
+    path('addfooditem', views.FoodItemPage, name='fooditem'),
+    path('deletefooditem/<str:pk>/', views.deleteFoodItem, name='deletefooditem'),
+    path('fooditem/<str:pk>/', views.foodItem, name='fooditem'),
     path('menu', views.menu, name='menu'),
     path('adminmenu', views.adminmenu, name='adminmenu'),
     path('updatefooditem/<str:pk>/', views.updateFoodItem, name='updatefooditem'),
+    #path('cart_summary', views.cart_summary, name='cart_summary'),
+    #path('cart_add', views.cart_add, name="cart_add"),
+    #path('cart_delete', views.cart_delete, name="cart_delete"),
+    #path('cart_update', views.cart_update, name="cart_update"),
 ]

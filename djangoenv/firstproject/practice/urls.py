71f3fdd2d43f308eb57b4312ed05a0fc337fc75b
YapJack7890 <108270479+YapJack7890@users.noreply.github.com>
2024-01-30 15:08:42 +0800

    Update urls.py

--- djangoenv/firstproject/practice/urls.py
+++ djangoenv/firstproject/practice/urls.py
@@ -1,21 +1,32 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    #Authentication
     path('register', views.registerPage, name='register'),
     path('logout', views.logoutUser, name='logout'),
-    path('home', views.home, name='home'),
     path('', views.loginPage, name='login'),
+
+    #Parent Page
+    path('home', views.home, name='home'),
     path('student', views.studentPage, name='student'),
-    path('request', views.RequestPage, name='request'),
-    path('addfooditem', views.FoodItemPage, name='fooditem'),
+    path('menu/<int:student_id>/', views.menu, name='menu'),
+    path('menu/', views.menu, name='menu_without_student'),
+    path('foodItem/<int:pk>/<int:student_id>/', views.foodItem, name='fooditem'),
+    path('add_to_cart/<str:pk>/', views.add_to_cart, name='add_to_cart'),
+
+    #Admin Page
+    path('addfooditem', views.FoodItemPage, name='addfooditem'),
     path('deletefooditem/<str:pk>/', views.deleteFoodItem, name='deletefooditem'),
-    path('fooditem/<str:pk>/', views.foodItem, name='fooditem'),
-    path('menu', views.menu, name='menu'),
-    path('adminmenu', views.adminmenu, name='adminmenu'),
     path('updatefooditem/<str:pk>/', views.updateFoodItem, name='updatefooditem'),
-    #path('cart_summary', views.cart_summary, name='cart_summary'),
-    #path('cart_add', views.cart_add, name="cart_add"),
-    #path('cart_delete', views.cart_delete, name="cart_delete"),
-    #path('cart_update', views.cart_update, name="cart_update"),
+    path('adminmenu', views.adminmenu, name='adminmenu'),
+    path('request_list', views.request_list, name='request_list'),
+    path('request_details/<str:pk>/', views.request_details, name='request_details'),
+
+    #Canteen Worker Page
+    path('request', views.RequestPage, name='request'),
+    path('vieworder', views.view_order, name='vieworder'),
+
+
 ]

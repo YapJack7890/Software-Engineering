fe3d849893711612b75ef0946ff513bdf64ae814
YapJack7890 <108270479+YapJack7890@users.noreply.github.com>
2024-01-30 15:09:31 +0800

    Update forms.py

--- djangoenv/firstproject/practice/forms.py
+++ djangoenv/firstproject/practice/forms.py
@@ -2,10 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
-from .models import Student, FoodItem, Request, Order, CanteenWorker
+from .models import Student, FoodItem, Request, CanteenWorker
 
 class CreateUserForm(UserCreationForm):
-    #email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     phone_number = forms.CharField(max_length=15, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}))
     class Meta:
         model = User
@@ -19,9 +18,14 @@ class StudentForm(ModelForm):
 class FoodItemForm(ModelForm):
     class Meta:
         model = FoodItem
-        fields = ('Food_Name', 'Food_Price', 'Ingredient_List', 'Food_Description', 'Food_Category')
+        fields = ('Food_Name', 'Food_Price', 'Ingredient_List', 'Food_Description')
 
 class RequestForm(ModelForm):
     class Meta:
         model = Request
-        fields = ('RequestFood_Name', 'RequestFood_Price', 'RequestIngredient_List', 'RequestFood_Description', 'RequestFood_Category', 'Request_Title')
+        fields = ('RequestFood_Name', 'RequestFood_Price', 'RequestIngredient_List', 'RequestFood_Description', 'Request_Title')
+
+class CanteenWorkerForm(ModelForm):
+    class Meta:
+        model = CanteenWorker
+        fields = ('Worker_Username', 'Worker_Password')

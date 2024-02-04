f77fbddf9032a1d928d8d5852ba4ddfe07245e83
YapJack7890 <108270479+YapJack7890@users.noreply.github.com>
2024-02-04 23:23:48 +0800

    Update admin.py

--- djangoenv/firstproject/practice/admin.py
+++ djangoenv/firstproject/practice/admin.py
@@ -10,8 +10,11 @@ admin.site.register(FoodItem)
 from .models import Request
 admin.site.register(Request)
 
+from .models import Cart
+admin.site.register(Cart)
+
+from .models import CartItem
+admin.site.register(CartItem)
+
 from .models import Order
 admin.site.register(Order)
-
-from .models import CanteenWorker
-admin.site.register(CanteenWorker)

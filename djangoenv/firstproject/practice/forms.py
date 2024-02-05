25d0ecdeb028685cbb95463ed6197143672ec779
YapJack7890 <108270479+YapJack7890@users.noreply.github.com>
2024-02-06 00:27:10 +0800

    Update forms.py

--- djangoenv/firstproject/practice/forms.py
+++ djangoenv/firstproject/practice/forms.py
@@ -5,10 +5,9 @@ from django.forms import ModelForm
 from .models import Student, FoodItem, Request
 
 class CreateUserForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=15, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}))
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
 class StudentForm(ModelForm):
     class Meta:
